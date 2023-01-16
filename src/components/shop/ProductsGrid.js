af192ed0db0f865b8e6053adf62d6e89dc4fa771
warith <warith.ad02@gmail.com>
2023-01-16 22:14:24 +0100

    Refactor ProductsGrid to use planets.json file to map planet products

--- src/components/shop/ProductsGrid.js
+++ src/components/shop/ProductsGrid.js
@@ -1,81 +1,29 @@
 import styled from "styled-components";
 import Product from "./Product";
+import Planets from "../../utils/planets.json"
 
-//write code for the nine planets as products
-//make their onaddtocart method return their names and prices up to the main component
 const ProductsGrid =({onAddToCart})=>{
     return(
         <ProductsGridWrapper>
-            <div>
-                <Product
-                    name='MERCURY'
-                    image=''
-                    price='20.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='VENUS'
-                    image=''
-                    price='20.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='EARTH'
-                    image=''
-                    price='50.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='MARS'
-                    image=''
-                    price='19.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='JUPITER'
-                    image=''
-                    price='14.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='SATURN'
-                    image=''
-                    price='12.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='URANUS'
-                    image=''
-                    price='25.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='NEPTUNE'
-                    image=''
-                    price='14.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                />
-                <Product
-                    name='PLUTO'
-                    image=''
-                    price='9.99'
-                    about=''
-                    onAddToCart={onAddToCart}
-                 />
-            </div>
+            {Planets.map(planet => {
+                return (
+                    <Product
+                        key={planet.id}
+                        id={planet.id}
+                        name={planet.name}
+                        price={planet.price}
+                        image={planet.img}
+                        about=''
+                        onAddToCart={onAddToCart}
+                    />
+                )
+            })}
         </ProductsGridWrapper>
     )
 }
 
 const ProductsGridWrapper = styled.div`
-
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
 `
 export default ProductsGrid;
